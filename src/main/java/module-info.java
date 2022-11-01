b6ffba2f83d3a37782dd1fb5e047ae1d727de1e4
Alexandr <a.piatrovich@gmail.com>
2022-11-01 18:15:37 +0300

    fix imports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module by.javafx.petrovich.demo {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     exports by.javafx.petrovich.demo;
+    exports by.javafx.petrovich.demo.model;
+    exports by.javafx.petrovich.demo.controller;
+
     opens by.javafx.petrovich.demo to javafx.fxml;
+    opens by.javafx.petrovich.demo.model to javafx.fxml;
+    opens by.javafx.petrovich.demo.controller to javafx.fxml;
 }
