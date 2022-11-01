5bfb5f50343a586ea750f6e015d38652ac8506c7
Alexandr <a.piatrovich@gmail.com>
2022-11-01 21:53:25 +0300

    optimize imports

--- src/main/java/by/javafx/petrovich/demo/EmployeeApplication.java
+++ src/main/java/by/javafx/petrovich/demo/EmployeeApplication.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class EmployeeApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
