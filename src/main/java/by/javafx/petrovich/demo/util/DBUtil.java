58bf01eb2926db467b352fc0f44566ce3c7b2eed
Alexandr <a.piatrovich@gmail.com>
2022-11-05 15:27:04 +0300

    optimize Imports

--- src/main/java/by/javafx/petrovich/demo/util/DBUtil.java
+++ src/main/java/by/javafx/petrovich/demo/util/DBUtil.java
@@ -9,15 +9,20 @@ import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
 
+import static by.javafx.petrovich.demo.dao.ColumnNames.ID_EMPLOYEE;
+import static by.javafx.petrovich.demo.dao.ColumnNames.PERSONNEL_NUMBER;
+import static by.javafx.petrovich.demo.dao.ColumnNames.NAME;
+import static by.javafx.petrovich.demo.dao.ColumnNames.SURNAME;
+
 
 public class DBUtil implements Initializable {
     private static final String JDBC_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
     private static final String CONNECTION_LINK = "jdbc:mysql://localhost:3306/employeesort";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "HzaArk_XnsS";
-    private static final String SQL_READ_ALL_EMPLOYEE = "SELECT idEmployees, name FROM employeesort.employees";
-    private static final String SQL_READ_EMPLOYEE_BY_ID = "SELECT idEmployees, name FROM employees where idEmployees = ?";
-    private static final String SQL_READ_EMPLOYEE_BY_NAME = "SELECT idEmployees, name FROM employees where name = ?";
+    private static final String SQL_READ_ALL_EMPLOYEE = "SELECT id_employee, personnel_number, name, surname FROM employeesort.employees;";
+    private static final String SQL_READ_EMPLOYEE_BY_ID = "SELECT id_employee, personnel_number, name, surname FROM employees where id_employee = ?";
+    private static final String SQL_READ_EMPLOYEE_BY_NAME = "SELECT id_employee, personnel_number, name, surname FROM employees where name = ?";
 
 
     public static Connection ConnectDd() {
@@ -44,8 +49,10 @@ public class DBUtil implements Initializable {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 Employee employee = new Employee();
-                employee.setIdEmployees(resultSet.getInt("idEmployees"));
-                employee.setName(resultSet.getString("name"));
+                employee.setIdEmployees(resultSet.getInt(ID_EMPLOYEE));
+                employee.setPersonnelNumber(resultSet.getInt(PERSONNEL_NUMBER));
+                employee.setName(resultSet.getString(NAME));
+                employee.setSurname(resultSet.getString(SURNAME));
                 allEmployees.add(employee);
             }
         } catch (SQLException e) {
@@ -63,8 +70,10 @@ public class DBUtil implements Initializable {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 Employee employee = new Employee();
-                employee.setIdEmployees(resultSet.getInt("idEmployees"));
-                employee.setName(resultSet.getString("name"));
+                employee.setIdEmployees(resultSet.getInt(ID_EMPLOYEE));
+                employee.setPersonnelNumber(resultSet.getInt(PERSONNEL_NUMBER));
+                employee.setName(resultSet.getString(NAME));
+                employee.setSurname(resultSet.getString(SURNAME));
                 allEmployees.add(employee);
             }
         } catch (SQLException e) {
@@ -82,8 +91,10 @@ public class DBUtil implements Initializable {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 Employee employee = new Employee();
-                employee.setIdEmployees(resultSet.getInt("idEmployees"));
-                employee.setName(resultSet.getString("name"));
+                employee.setIdEmployees(resultSet.getInt(ID_EMPLOYEE));
+                employee.setPersonnelNumber(resultSet.getInt(PERSONNEL_NUMBER));
+                employee.setName(resultSet.getString(NAME));
+                employee.setSurname(resultSet.getString(SURNAME));
                 allEmployees.add(employee);
             }
         } catch (SQLException e) {
