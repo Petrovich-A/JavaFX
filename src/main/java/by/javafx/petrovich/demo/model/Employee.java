42edaf7bba9a32063a9172014c622cdff77c8d3c
Alexandr <a.piatrovich@gmail.com>
2022-11-05 14:18:33 +0300

    add personnelNumber n surname fields

--- src/main/java/by/javafx/petrovich/demo/model/Employee.java
+++ src/main/java/by/javafx/petrovich/demo/model/Employee.java
@@ -4,14 +4,18 @@ import java.util.Objects;
 
 public class Employee {
     int idEmployees;
+    int personnelNumber;
     String name;
-
-    public Employee(int idEmployees, String name) {
-        this.idEmployees = idEmployees;
-        this.name = name;
-    }
+    String surname;
 
     public Employee() {
+    }
+
+    public Employee(int idEmployees, int personnelNumber, String name, String surname) {
+        this.idEmployees = idEmployees;
+        this.personnelNumber = personnelNumber;
+        this.name = name;
+        this.surname = surname;
     }
 
     public int getIdEmployees() {
@@ -22,6 +26,14 @@ public class Employee {
         this.idEmployees = idEmployees;
     }
 
+    public int getPersonnelNumber() {
+        return personnelNumber;
+    }
+
+    public void setPersonnelNumber(int personnelNumber) {
+        this.personnelNumber = personnelNumber;
+    }
+
     public String getName() {
         return name;
     }
@@ -30,24 +42,34 @@ public class Employee {
         this.name = name;
     }
 
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return idEmployees == employee.idEmployees && Objects.equals(name, employee.name);
+        return idEmployees == employee.idEmployees && personnelNumber == employee.personnelNumber && Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(idEmployees, name);
+        return Objects.hash(idEmployees, personnelNumber, name, surname);
     }
 
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("Employee{");
         sb.append("idEmployees=").append(idEmployees);
+        sb.append(", personnelNumber=").append(personnelNumber);
         sb.append(", name='").append(name).append('\'');
+        sb.append(", surname='").append(surname).append('\'');
         sb.append('}');
         return sb.toString();
     }
