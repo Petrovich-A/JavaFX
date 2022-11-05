e19d0752382f8b05e4590683fa939bc50a29f098
Alexandr <a.piatrovich@gmail.com>
2022-11-05 14:17:52 +0300

    create ColumnLabels.java

---
+++ src/main/java/by/javafx/petrovich/demo/dao/ColumnLabels.java
@@ -0,0 +1,9 @@
+package by.javafx.petrovich.demo.dao;
+
+public class ColumnLabels {
+    public static final String ID_EMPLOYEE = "id_employee";
+    public static final String PERSONNEL_NUMBER = "personnel_number";
+    public static final String NAME = "name";
+    public static final String SURNAME = "surname";
+
+}
