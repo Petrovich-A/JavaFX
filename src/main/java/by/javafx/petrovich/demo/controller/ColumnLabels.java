4d41c8dfcb0e7861413de032658ceb0845dbe2ef
Alexandr <a.piatrovich@gmail.com>
2022-11-07 23:34:11 +0300

    renaming

--- src/main/java/by/javafx/petrovich/demo/controller/ColumnLabels.java
+++
@@ -1,9 +0,0 @@
-package by.javafx.petrovich.demo.controller;
-
-public class ColumnLabels {
-    public static final String ID_EMPLOYEE = "id";
-    public static final String PERSONNEL_NUMBER = "Personnel number";
-    public static final String NAME = "Name";
-    public static final String SURNAME = "Surname";
-
-}
---
+++ src/main/java/by/javafx/petrovich/demo/controller/EmployeeFieldsNames.java
@@ -0,0 +1,9 @@
+package by.javafx.petrovich.demo.controller;
+
+public class EmployeeFieldsNames {
+    public static final String ID_EMPLOYEE = "idEmployees";
+    public static final String PERSONNEL_NUMBER = "personnelNumber";
+    public static final String NAME = "name";
+    public static final String SURNAME = "surname";
+
+}
