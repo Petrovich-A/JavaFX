b1feefd1e459a445fe12fe49e30aa1ec5c7f413b
Alexandr <a.piatrovich@gmail.com>
2022-11-08 19:24:46 +0300

    add ChoiceBoxItemNames.java

---
+++ src/main/java/by/javafx/petrovich/demo/controller/ChoiceBoxItemNames.java
@@ -0,0 +1,8 @@
+package by.javafx.petrovich.demo.controller;
+
+public class ChoiceBoxItemNames {
+        public static final String ID_ITEM = "id";
+        public static final String PERSONNEL_NUMBER_ITEM = "personnel number";
+        public static final String NAME_ITEM = "name";
+        public static final String SURNAME_ITEM = "surname";
+}
