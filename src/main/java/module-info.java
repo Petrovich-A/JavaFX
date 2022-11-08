6e68d8411b1a5f75b52c47d4c4a4143cc1497878
Alexandr <a.piatrovich@gmail.com>
2022-11-08 17:32:26 +0300

    add logger

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module by.javafx.petrovich.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.apache.logging.log4j;
 
     exports by.javafx.petrovich.demo;
     exports by.javafx.petrovich.demo.model;
