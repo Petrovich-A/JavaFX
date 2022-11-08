fa6a81d8f683952dd9fb37bba93c6a8a55dc8ce0
Alexandr <a.piatrovich@gmail.com>
2022-11-08 21:28:57 +0300

    add alert messages

--- src/main/java/by/javafx/petrovich/demo/controller/AlertMessages.java
+++ src/main/java/by/javafx/petrovich/demo/controller/AlertMessages.java
@@ -4,5 +4,7 @@ public class AlertMessages {
     public static final String INPUT_NUMBER = "Input a number";
     public static final String CHOICE_AND_FILL = "Choice an item and fill a text field";
     public static final String NO_RESULTS = "no results";
+    public static final String NO_CHOICE_ITEM = "no choice item in dropdown list";
+    public static final String NO_INPUT_VALUE = "no value in text field";
 
 }
