c72af43f8c0a45432a1ffe26f55e6cf3a6766e8d
Alexandr <a.piatrovich@gmail.com>
2022-11-10 22:49:17 +0300

    add an app icon

--- src/main/java/by/javafx/petrovich/demo/EmployeeApplication.java
+++ src/main/java/by/javafx/petrovich/demo/EmployeeApplication.java
@@ -3,19 +3,24 @@ package by.javafx.petrovich.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class EmployeeApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(EmployeeApplication.class.getResource("employeeSort.fxml"));
+        Image image = new Image("bussiness-man.png");
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setScene(scene);
         stage.setTitle("Employee app");
+        stage.getIcons().add(image);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
     }
+
 }
