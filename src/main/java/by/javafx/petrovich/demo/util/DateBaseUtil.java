ce1fbcd475c0b61091f5e2fb5909ae90bcc897a0
Petrovich-A <A.Piatrovich@gmail.com>
2022-11-11 12:12:51 +0300

    made data loading for database connection from property files :floppy_disk:

--- src/main/java/by/javafx/petrovich/demo/util/DateBaseUtil.java
+++ src/main/java/by/javafx/petrovich/demo/util/DateBaseUtil.java
@@ -9,32 +9,35 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.ResourceBundle;
+
 public class DateBaseUtil implements Initializable {
     private static final Logger LOGGER = LogManager.getLogger();
     private static Connection connection = null;
-    private static final String JDBC_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-    private static final String CONNECTION_LINK = "jdbc:mysql://localhost:3306/employeesort";
-    private static final String USER_NAME = "root";
-    private static final String PASSWORD = "HzaArk_XnsS";
+    private static final PropertyLoader PROPERTY_LOADER = new PropertyLoader();
+    private static final String JDBC_DRIVER_NAME = PROPERTY_LOADER.get("JDBC_DRIVER_NAME");
+    private static final String CONNECTION_URL = PROPERTY_LOADER.get("CONNECTION_URL");
+    private static final String USER_NAME = PROPERTY_LOADER.get("USER_NAME");
+    private static final String PASSWORD = PROPERTY_LOADER.get("PASSWORD");
+
     public Connection receiveConnection() {
         if (connection != null) {
             return connection;
         } else {
             try {
                 Class.forName(JDBC_DRIVER_NAME);
-                Connection connection = DriverManager.getConnection(CONNECTION_LINK, USER_NAME, PASSWORD);
-                LOGGER.log(Level.INFO, String.format("Connection established."));
+                Connection connection = DriverManager.getConnection(CONNECTION_URL, USER_NAME, PASSWORD);
+                LOGGER.log(Level.INFO, "Connection established.");
                 return connection;
             } catch (Exception e) {
-                LOGGER.log(Level.ERROR, String.format("Connection Failed!"), e);
+                LOGGER.log(Level.ERROR, "Connection Failed!", e);
                 e.printStackTrace();
             }
             return connection;
         }
     }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
-
 
 }
