1849591d8aaa6b5c6138c054e98f91b034d22e8f
Petrovich-A <A.Piatrovich@gmail.com>
2022-11-11 12:10:59 +0300

    made data loading for database connection from property files :floppy_disk:

---
+++ src/main/java/by/javafx/petrovich/demo/util/PropertyLoader.java
@@ -0,0 +1,39 @@
+package by.javafx.petrovich.demo.util;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyLoader {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final String PROPERTY_PATH = "jdbc-sql-config.properties";
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        InputStream inputStream = PropertyLoader.class.getClassLoader().getResourceAsStream(PROPERTY_PATH);
+        if (inputStream == null) {
+            LOGGER.log(Level.FATAL, String.format("%s file does not exist, initialization failed.", PROPERTY_PATH));
+        }
+        readProperties(inputStream);
+    }
+
+    private static void readProperties(InputStream inputStream) {
+        try {
+            PROPERTIES.load(inputStream);
+        } catch (FileNotFoundException e) {
+            LOGGER.log(Level.FATAL, String.format("Properties file not found: %s. %s", e.getMessage(), e));
+        } catch (IOException e) {
+            LOGGER.log(Level.FATAL, String.format("Reading database properties failed: %s. %s", e.getMessage(), e));
+        }
+        LOGGER.log(Level.INFO, "Reading property file successful.");
+    }
+
+    public String get(String propertyName) {
+        return PROPERTIES.getProperty(propertyName);
+    }
+}
