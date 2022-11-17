c508ff93f5870d85f4c7b8950230cc7c70a64f86
Alexandr <a.piatrovich@gmail.com>
2022-11-17 22:15:23 +0300

    delete FileNotFoundException

--- src/main/java/by/javafx/petrovich/demo/util/PropertyLoader.java
+++ src/main/java/by/javafx/petrovich/demo/util/PropertyLoader.java
@@ -1,13 +1,12 @@
 package by.javafx.petrovich.demo.util;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author Petrovich A.V.
@@ -20,7 +19,8 @@ public class PropertyLoader {
     static {
         InputStream inputStream = PropertyLoader.class.getClassLoader().getResourceAsStream(PROPERTY_PATH);
         if (inputStream == null) {
-            LOGGER.log(Level.FATAL, "File does not exist, initialization failed.", PROPERTY_PATH);
+            LOGGER.log(Level.ERROR, "File does not exist, initialization failed.", PROPERTY_PATH);
+            throw new RuntimeException("File does not exist, initialization failed.");
         }
         readProperties(inputStream);
     }
@@ -33,8 +33,6 @@ public class PropertyLoader {
     private static void readProperties(InputStream inputStream) {
         try {
             PROPERTIES.load(inputStream);
-        } catch (FileNotFoundException e) {
-            LOGGER.log(Level.ERROR, "Properties file not found", e.getMessage());
         } catch (IOException e) {
             LOGGER.log(Level.ERROR, "Reading database properties failed", e.getMessage());
         }
@@ -42,8 +40,8 @@ public class PropertyLoader {
     }
 
     /**
-     * @param propertyName name of key in Properties file
-     * @return receive value
+     * @param propertyName name of key in properties file
+     * @return received value from properties file
      */
     public String receive(String propertyName) {
         return PROPERTIES.getProperty(propertyName);
